bfa1e0ac554edf63fb4e0ce15c86cd91afe15821
Amrit Gautam <40325188+aga01935@users.noreply.github.com>
2021-04-03 00:27:48 -0500

    Update Minimizer_Multidim.py

--- Minimizer_Multidim.py
+++ Minimizer_Multidim.py
@@ -20,7 +20,7 @@ fval = 2*x**4-6*x**3+8*y**3+ 7*y**2+7
 min= fmin(myfunc,[2,2])
 fmin = 2*min[0]**4-6*min[0]**3+8*min[1]**3+ 7*min[1]**2+7
 
-print ("Minimum of the function is : ", min)#"(",min.x[0],min.y[0],min.fun,")" )
+print ("Minimum of the function is at : ", min, "Minimum value of function is ",fmin)#"(",min.x[0],min.y[0],min.fun,")" )
 #print ("Fun", func)
 #plt.figure()
 #plt.plot(xy2,myfunc(xy),"r",label ="Function")
